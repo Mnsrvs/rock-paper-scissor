a00812ddaf1f0ee94e15a4d8d2a2e155511eb1c8
Charles Monsarvas <charles.monsarvas@gmail.com>
2022-05-20 17:42:19 +0800

    Add variable playerSelection
    Assign the prompt method value to playerSelection variable

--- script.js
+++ script.js
@@ -39,6 +39,6 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    
+    let playerSelection = prompt('Enter rock paper scissors', '');
   }
 }
