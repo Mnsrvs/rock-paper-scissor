ac3c85046e61ac8876a4c94eca2eeac6ec5628db
Charles Monsarvas <charles.monsarvas@gmail.com>
2022-05-20 15:27:33 +0800

    Add function playRound()
    Add function playRound() with parameters playerSelection and
    computerSelection

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function computerPlay() {
   const choices = ['rock', 'paper', 'scissors'];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound(palyerSelection, computerSelection) {
+  
+}
