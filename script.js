22754a69d31cf9a0e8f8eac0d77d99624ee786a1
Charles Monsarvas <charles.monsarvas@gmail.com>
2022-05-20 15:45:35 +0800

    Add if else statement
    Add if else statement for playerSelection and computerSelection

--- script.js
+++ script.js
@@ -4,5 +4,35 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  
+  if (playerSelection === computerSelection) {
+    return 'Tie game!';
+  }
+
+  if (playerSelection === 'rock') {
+    if (computerSelection === 'scissors') {
+      return 'Player won! rock beats scissors';
+    } else {
+      return 'Computer won! paper beats rock';
+    }
+  }
+
+  if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      return 'Player won! paper beats rock';
+    } else {
+      return 'Computer won! scissors beats rock';
+    }
+  }
+
+  if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') {
+      return 'Player won! scissors beats paper';
+    } else {
+      return 'Computer won! rock beats scissors';
+    }
+  }
+
+  if (playerSelection !== 'rock' || 'paper' || 'scissors' || null) {
+    return 'Invalid input';
+  }
 }
